d37fbe129a35b193dc859764fc62a2b57c73655d
Dulangi2002 <cb010550@students.apiit.lk>
2024-04-13 13:37:40 +0530

    test proxt 13

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,8 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
+import { createProxyMiddleware } from 'http-proxy-middleware'
 
-module.exports = function (app) {
+export default function (app) {
   app.use(
-    '/api',
+    '/api/sch/payments',
     createProxyMiddleware({
       target: 'https://secure.myfees.lk',
       changeOrigin: true,
