f169c1a6e7024832560f5dc2ad7df559b6090342
Dulangi2002 <cb010550@students.apiit.lk>
2024-04-28 12:11:11 +0530

    new1

--- api/server.js
+++ api/server.js
@@ -3,9 +3,13 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import got from 'got';
 import bodyParser from 'body-parser';
+import { Stream } from 'stream';
+import axios from 'axios';
+import { type } from 'os';
 
 const app = express();
 app.use(bodyParser.json());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const API_SERVICE_URL = 'https://secure.myfees.lk';
@@ -27,34 +31,24 @@ console.log(publicDirectoryPath)
 
 router.post('/api', async (req, res) => {
   try {
-    // const response = await got.post(`${API_SERVICE_URL}/api/sch/payments`, {
-    //   json: req.body,
-    //   responseType: 'json',
-    // });
-    
-    // console.log(response.body);
-    // console.log('External Server Status Code:', response.statusCode);
-    // if(response.statusCode === 201){
-    // const jsonRes =JSON.stringify(response.body);
-    // res.send(jsonRes);
-    // }else{
-    //   res.status(response.statusCode).json({ error: "Unexpected status code", statusCode: response.statusCode });
-    // }
-
-
-    const response =  got.stream.post(`${API_SERVICE_URL}/api/sch/payments`, {
+    const response = await got.post(`${API_SERVICE_URL}/api/sch/payments`, {
       json: req.body,
       responseType: 'json',
-    });
+      headers: {
+        'Content-Type': 'application/json',
+      },
 
-    response.pipe(res);
-    if (res.statusCode !== 200) {
-      res.status(response.statusCode).json({ error: "External Server Error", statusCode: response.statusCode });
-      res.send();
+    });
+    
+    console.log(response.body);
+    console.log('External Server Status Code:', response.statusCode);
+    if(response.statusCode === 201){
+    const jsonRes =JSON.stringify(response.body);
+    res.send(jsonRes);
+    }else{
+      res.status(response.statusCode).json({ error: "Unexpected status code", statusCode: response.statusCode });
     }
-    else{
-      res.status(200).json({ message: "Success" });
-    }
+
     
   } catch (error) {
     console.error(error);
